0ede3f67216ffa1e93e59ddc569ad13bb0e41290
Stephen Wallis <stephen.wallis@ami.co.nz>
2014-02-19 10:40:42 +1300

    naming

--- NotQuiteAzure/NotQuiteAzure/Service1.svc.cs
+++ NotQuiteAzure/NotQuiteAzure/Service1.svc.cs
@@ -9,7 +9,7 @@ using System.Text;
 namespace NotQuiteAzure
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    public class Service1 : IService1
+    public class NotQuiteAzure : INotQuiteAzure
     {
         public string GetData(int value)
         {
