b850f012c7672dcec03fd9b94d416f69c7a228f3
Stephen Wallis <stephen.wallis@ami.co.nz>
2014-02-19 15:10:42 +1300

    moq

--- NotQuiteAzure/NotQuiteAzure.Tests/RegisterTests.cs
+++ NotQuiteAzure/NotQuiteAzure.Tests/RegisterTests.cs
@@ -3,15 +3,22 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace NotQuiteAzure.Tests
 {
     [TestClass]
     public class RegisterTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        Mock<DatabaseConnection> databaseConnection = new Mock<DatabaseConnection>();
+
+        //[TestMethod]
+        //public void TestRegister()
+        //{
+        //    databaseConnection.Setup(d => d.GetCustomer(It.IsAny<string>())).Returns(new Customer() { id = "testeroo" });
+
+        //    NotQuiteAzure notQuiteAzure = new NotQuiteAzure();
+        //    notQuiteAzure.Register("testId");
+        //}
     }
 }
